21a114a7940c7a61d67190b48293e2602e364c51
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-07-25 10:57:27 +0800

    Feature Update: Add more content and styling for home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,6 +10,10 @@ const Home = () => {
           I'm <span className={styles.label}>Javier Wong.</span>
         </h1>
         <h1>I design and build things</h1>
+        <div className={styles.separator}></div>
+        <p className={styles.paragraph}>
+          I'm a junior developer based in Singapore specializing in building websites and applications.
+        </p>
       </div>
     </div>
   )
