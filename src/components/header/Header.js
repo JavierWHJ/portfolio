3a3fa968b9ccf77c8f660e0eee6595cf6ede06b3
Javier <54625014+JavierWHJ@users.noreply.github.com>
2021-05-03 16:08:13 +0800

    Update resume button styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,29 +4,37 @@ import React from "react"
 import styles from "./Header.module.scss"
 import smallLogo from '../../images/logo_white_small.svg';
 
-const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
-    <div className={styles.logoContainer}>
-      <Link to="/">
-        <img src={smallLogo} width="80" alt="small logo"/>
-      </Link>
-    </div>
-    <div className={styles.navbar}>
-      <Link to="/" className={styles.navlink}>
-        .about()
-      </Link>
-      <Link to="/" className={styles.navlink}>
-        .work()
-      </Link>
-      <Link to="/" className={styles.navlink}>
-        .projects()
-      </Link>
-      <Link to="/" className={styles.navlink}>
-        .contact()
-      </Link>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+
+  const goToResume = () => {
+    window.open('/resume.pdf')
+  }
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Link to="/">
+          <img src={smallLogo} width="80" alt="small logo"/>
+        </Link>
+      </div>
+      <div className={styles.navbar}>
+        <Link to="/" className={styles.navlink}>
+          .about()
+        </Link>
+        <Link to="/" className={styles.navlink}>
+          .work()
+        </Link>
+        <Link to="/" className={styles.navlink}>
+          .projects()
+        </Link>
+        <Link to="/" className={styles.navlink}>
+          .contact()
+        </Link>
+        <button onClick={goToResume}>Resume</button>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
